7be07ac6886da53d7086351871a27b55d28a519f
Alan Lu <alu36@ucsc.edu>
2024-04-08 23:15:44 -0700

    experiment 1 done

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,6 +1,6 @@
 // project.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Alan Lu
+// Date: 4/7/2023
 
 // NOTE: This is how we might start a basic JavaaScript OOP project
 
@@ -23,12 +23,55 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    temp: ["Hot", "Cold", "Luke-warm", "Freezing-cold", "Boiling-hot"],
+    drink: ["drink", "beverage", "liquid", "stuff"],
+    name: ["Steve", "Max", "Ari", "William", "Vex", "Grace", "Megan", "Jasmine", "Queenie"],
+    type: ["Milk tea", "Coffee", "Thai Tea", "Apple Juice", "Orange Juice", "Blood Cocktail"],
+    item1: ["boba", "lychee jelly", "apple chunks", "orange peels", "eyeballs", "egg pudding"],
+    num: ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Infinite", "Too many"],
+    item2: ["extra sugar", "sea salt", "cream", "coagulated blood", "no sugar", "half and half"],
+    move: ["walk", "step", "crawl", "roll", "hop", "dance"],
+    food: ["chips", "cookies", "fruits", "chunks of various organs", "crackers", "fried chicken", "tater tots"],
+    num1: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    currency: ["dollars", "yen", "gold coins", "yuan", "rupees", "human skulls"],
+    
+  };
+  
+  const template = ` \nSnack Made! \n\n $temp $drink for $name! \n $num $type with $item1 and $item2. \n 
+  Please $move your way up to the counter and grab your $drink. \n Would you like anything with that? \n We have a deal on $food today. 
+  \n That will be the low low price of $num1$num1 $currency! \n Thank You!
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    box.innerText = story;
+  }
+  
+  /* global clicker */
+  clicker.onclick = generate;
+  
+  generate();
+  
 }
 
-// let's get this party started - uncomment me
-//main();
+main();
